271d742aad53c50903ff1a7e1ceb11e1ef14dc77
JoraBarjomi <vip.nero228@mail.ru>
2026-03-14 13:49:41 +0700

    add logs page for deploy

--- src/pages/alarms/index.tsx
+++ src/pages/alarms/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export function AlarmsPage() {
   return (
     <div>
---
+++ src/pages/logs/index.tsx
@@ -0,0 +1,7 @@
+export function LogsPage() {
+  return (
+    <div>
+      <h2>This is the Logs page</h2>
+    </div>
+  );
+}
